9e7d0521d51308b5aec15c6fb8d792e582cda7da
JMaling <jmaling@fwparker.org>
2018-05-18 08:49:48 -0500

    Twitter Scraper V2

--- Twitter_Scraper.py
+++ Twitter_Scraper.py
@@ -2,22 +2,29 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 
-url = "https://twitter.com/search?q=" + "celebrityname" + "&src=tyah&lang=en"
-driver = webdriver.Chrome()
-driver.get(url)
-time.sleep(5)
+def twitter_scraper(first_name, last_name):
+    url = "https://twitter.com/search?q=" + first_name + "%20" + last_name + "&src=tyah&lang=en"
+    print(url)
+    driver = webdriver.Chrome()
+    driver.get(url)
+    time.sleep(5)
 
-for i in range(5):
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-    time.sleep(3)
+    for i in range(5):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+        time.sleep(3)
 
-htmlSource = driver.page_source
-soup = BeautifulSoup(htmlSource, "html.parser")
-tweets = soup.findAll("p", class_="tweet-text")
+    htmlSource = driver.page_source
+    soup = BeautifulSoup(htmlSource, "html.parser")
+    tweets = soup.findAll("p", class_="tweet-text")
 
-words = [x.text.split() for x in tweets]
+    words = [x.text.split() for x in tweets]
 
-just_words = []
-for word in words:
-    just_words += word
-print(just_words)
+    just_words = []
+    for word in words:
+        just_words += word
+    word_list1 = [word.strip() for word in just_words if ".com" not in word]
+    word_list2 = [word for word in word_list1 if "#" not in word]
+    word_list3 = [word for word in word_list2 if "http" not in word]
+    word_list4 = [word for word in word_list3 if "@" not in word]
+    print(word_list3)
+twitter_scraper("John", "Lennon")
