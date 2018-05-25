19ea5b67dec108dde75408bd58cfa333b5d64b62
oliviaposner <oposner@fwparker.org>
2018-05-25 08:14:11 -0500

    yo

--- input_functions.py
+++ input_functions.py
@@ -1,28 +1,26 @@
-def word_process(positive_word_list, word_list6):
-    good = 0
-    good_words = []
+def get_good_list(sentiment_list, word_list6):
+    '''
+    :param positive_word_list: uci list of good words or bad words
+    :param word_list6: twitter feed list of words
+    :return: list of good or bad words found in twitter words
+    '''
+    total = 0
+    words = []
 
     for word in word_list6:
-        if word in positive_word_list:
-            good += 1
-            good_words.append(word)
+        if word in sentiment_list:
+            total += 1
+            words.append(word)
 
-    return good_words
-
-
-def bad_word_count(negative_word_list, word_list6):
-    bad = 0
-    bad_words = []
-
-    for word in word_list6:
-        if word in negative_word_list:
-            bad += 1
-            bad_words.append(word)
-
-    return bad_words
+    return words, total
 
 
-def percentages(good, bad):
+
+
+
+def percentages(good_words, bad_words):
+    good = len(good_words)
+    bad = len(bad_words)
     positive_percentage = ((good)/(good + bad)) * 100
 
     negative_percentage = 100 - positive_percentage
@@ -30,31 +28,15 @@ def percentages(good, bad):
     return negative_percentage, positive_percentage
 
 
-def frequent_words(word_list6):
+def frequent_words(my_list):
 
-    repeats = [[word, word_list6.count(word)] for word in word_list6]
+    repeats = [[word, my_list.count(word)] for word in my_list]
     print(repeats.sort(key=lambda x: x[1]))
     repeats = dict(repeats)
     repeats = list(repeats)
-    repeats = repeats[-3:]
+    #repeats = repeats[-3:]
     return repeats
 
-def frequent_positive(good_words):
-    good_repeats = [[word, good_words.count(word)] for word in good_words]
-    print(good_repeats.sort(key=lambda x: x[1]))
-    good_repeats = dict(good_repeats)
-    good_repeats = list(good_repeats)
-    good_repeats = good_repeats[-3:]
-    print(good_repeats)
-
-def frequent_negative(bad_words):
-    bad_repeats = [[word, bad_words.count(word)] for word in bad_words]
-    print(bad_repeats.sort(key=lambda x: x[1]))
-    bad_repeats = dict(bad_repeats)
-    bad_repeats = list(bad_repeats)
-    bad_repeats = bad_repeats[-3:]
-    print(bad_repeats)
 
 
-if __name__ == "__main__":
-    
+
